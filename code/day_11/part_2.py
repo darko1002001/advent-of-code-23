4cf0eaf776a2a43ac2687d4cf759dd51a378d281
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2023-12-11 11:51:16 +0100

    day 11

--- code/day_11/part_2.py
+++ code/day_11/part_2.py
@@ -6,14 +6,12 @@ def find_empty(inputs):
     filled_rows = defaultdict(lambda: False)
     filled_cols = defaultdict(lambda: False)
     galaxies = []
-    index = 1
     for r, row in enumerate(inputs):
         for c, char in enumerate(row):
             if char == "#":
                 filled_rows[r] = True
                 filled_cols[c] = True
-                galaxies.append((r, c, index))
-                index += 1
+                galaxies.append((r, c))
     return filled_rows, filled_cols, galaxies
 
 
@@ -30,20 +28,18 @@ def extra_space(i, j, filled) -> int:
 
 def distance(pair, filled_rows, filled_cols):
     first, second = pair
-    row1, col1, num1 = first
-    row2, col2, num2 = second
-    distance = abs(row1 - row2) + abs(col1 - col2)
+    row1, col1 = first
+    row2, col2 = second
     total = (
-        distance
+        abs(row1 - row2)
+        + abs(col1 - col2)
         + extra_space(row1, row2, filled_rows)
         + extra_space(col1, col2, filled_cols)
     )
-    # print(f"{num1}-{num2} | {distance=} {pair=}, {total=}")
     return total
 
 
 def solve(inputs) -> int:
     filled_rows, filled_cols, galaxies = find_empty(inputs)
     pairs = list(combinations(galaxies, 2))
-    # print(len(pairs))
     return sum([distance(pair, filled_rows, filled_cols) for pair in pairs])
